19dd6c461cc94f3db98100e7839ba05237f6566e
Adrian Herrera <adrian.herrera02@gmail.com>
2018-10-24 08:48:54 +1100

    fuzzalloc: get return address

--- fuzzalloc/src/malloc/malloc.c
+++ fuzzalloc/src/malloc/malloc.c
@@ -1,21 +1,31 @@
-#include <stdio.h>
+#if !defined(__x86_64__)
+#error Unsupported platform
+#endif
 
-__attribute__((constructor))
-static void __init_memory_allocator(void) {
-    printf("hello, constructor\n");
-}
+#include <stdio.h>
+#include <unistd.h>
 
 void *malloc(size_t size) {
+    void *return_addr = __builtin_return_address(0);
+    fprintf(stderr, "hello, malloc from %p\n", return_addr);
+
     return NULL;
 }
 
 void *calloc(size_t nmemb, size_t size) {
+    void *return_addr = __builtin_return_address(0);
+    fprintf(stderr, "hello, calloc from %p\n", return_addr);
+
     return NULL;
 }
 
 void *realloc(void *ptr, size_t size) {
+    void *return_addr = __builtin_return_address(0);
+    fprintf(stderr, "hello, realloc from %p\n", return_addr);
+
     return ptr;
 }
 
 void free(void *ptr) {
+    fprintf(stderr, "hello, free\n");
 }
