66607d2222d68683d69c25bc4a1d46e297ef4084
Adrian Herrera <adrian.herrera02@gmail.com>
2019-08-11 11:44:59 +1000

    spec cpu2006: added profiling hooks

--- spec-cpu2006-config/setup.sh
+++ spec-cpu2006-config/setup.sh
@@ -8,7 +8,7 @@ AFL_PATH=${SRC_DIR}/../afl-2.52b
 CONFIG_DIR=${SRC_DIR}/config
 
 # Activate SPEC CPU2006 environment
-source ./shrc
+source ${ROOT_DIR}/shrc
 
 # Get AFL
 export AFL_PATH
@@ -24,7 +24,13 @@ mkdir -p fuzzalloc-debug && \
   (cd fuzzalloc-debug && cmake -DFUZZALLOC_USE_LOCKS=True -DAFL_INSTRUMENT=On -DCMAKE_BUILD_TYPE=Debug ${SRC_DIR}/../fuzzalloc &&
   make -j)
 
-# Add config files
+# Add helper scripts
+ln -sf ${SRC_DIR}/../scripts fuzzalloc-scripts
+
+# Add config files and cleanup previous results
 for CONFIG in $(ls ${CONFIG_DIR}); do
   cp -f "${CONFIG_DIR}/${CONFIG}" "config/${CONFIG}"
+  runspec --config ${CONFIG} --action=scrub all
 done
+
+echo "Setup complete! Now run runspec --config=fuzzalloc --tune=base --noreportable all_except_fortran"
