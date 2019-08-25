d3dd73a258e3cba8227b1f2156ab589ebb8bebb8
Adrian Herrera <adrian.herrera02@gmail.com>
2019-08-26 08:52:02 +1000

    fuzzer-test-suite: update build scripts

--- fuzzer-test-suite/build-everything-afl.sh
+++ fuzzer-test-suite/build-everything-afl.sh
@@ -15,15 +15,21 @@ mkdir ${PARENT_DIR}
 echo "Created top directory ${PARENT_DIR}"
 
 build_target() {
-  file_name="$(basename ${1})"
+  TARGET="$(basename ${1})"
 
   [[ ! -d ${1} ]] && return
-  [[ ! -e ${1}build.sh ]] && return
+  [[ ! -e ${1}/build.sh ]] && return
 
-  echo "Running build ${file_name}"
+  # Build target
+  echo "Building ${TARGET}"
   cd ${PARENT_DIR}
-  ${ABS_SCRIPT_DIR}/build.sh "${file_name}" > ${file_name}.log 2>&1
+  ${ABS_SCRIPT_DIR}/build.sh "${TARGET}" > ${TARGET}.log 2>&1
+
+  # Create the empty seed
+  mkdir -p ${PARENT_DIR}/RUNDIR-${FUZZING_ENGINE}-${TARGET}/empty-seed
+  echo "" > ${PARENT_DIR}/RUNDIR-${FUZZING_ENGINE}-${TARGET}/empty-seed/seed
 }
 
 export -f build_target
-parallel -j10 build_target ::: ${ABS_SCRIPT_DIR}/*/
+BENCHMARKS="${ABS_SCRIPT_DIR}/*/"
+parallel build_target ::: "${BENCHMARKS}"
