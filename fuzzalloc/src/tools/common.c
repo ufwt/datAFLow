87d00193e97bec43cb599f1c53a4a43c388265c0
Adrian Herrera <adrian.herrera02@gmail.com>
2019-08-28 15:19:20 +1000

    llvm: ignore .S files

--- fuzzalloc/src/tools/common.c
+++ fuzzalloc/src/tools/common.c
@@ -14,7 +14,7 @@ u8 check_if_assembler(u32 argc, const char **argv) {
     u8 *cur = *(++argv);
 
     const u8 *ext = strrchr(cur, '.');
-    if (ext && !strcmp(ext + 1, "s")) {
+    if (ext && (!strcmp(ext + 1, "s") || !strcmp(ext + 1, "S"))) {
       return 1;
     }
   }
