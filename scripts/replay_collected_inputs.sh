616f8e62d058b0fd8d9e6815252b50c71002cbb5
Adrian Herrera <adrian.herrera02@gmail.com>
2019-09-14 14:23:48 +1000

    scripts: replay script outputs in CSV-ish format

--- scripts/replay_collected_inputs.sh
+++ scripts/replay_collected_inputs.sh
@@ -21,11 +21,11 @@ replay_inputs() {
   for I in 0 25001 50001 75001 100001 125001 150001 175001 200001 225001 250001 275001 300001 325001 350001 375001 400001 425001 450001 475001; do
     INPUTS=$(find ${INPUT_DIR} -type f | sort | tail -n +$I | head -25000)
     perf stat -r3 --output ${REPLAY_LOG} --append  -- \
-      "${RUNDIR}/${TARGET}-${FUZZING_ENGINE}" ${INPUTS} > /dev/null 2>&1
+      "${RUNDIR}/${TARGET}-${FUZZING_ENGINE}" ${INPUTS} 1> "${TARGET}-${FUZZING_ENGINE}-out.log" 2> "${TARGET}-${FUZZING_ENGINE}-err.log"
   done
 
   TOTAL_TIME=$(awk -F ' ' '$0 ~ /task-clock/ {TOTAL_TIME += $1} END {printf "%f", TOTAL_TIME}' ${REPLAY_LOG})
-  echo "${TARGET} replay time = ${TOTAL_TIME} ms"
+  echo "${TARGET}, ${TOTAL_TIME}"
 }
 
 export -f replay_inputs
