b4e012c13f1c1e377984634d09a4d233ea484345
Adrian Herrera <adrian.herrera02@gmail.com>
2020-01-19 10:55:04 +1100

    fuzzing-targets: refactored Docker stuff
    Now no longer fuzz in container.

--- fuzzing-targets/README.md
+++ fuzzing-targets/README.md
@@ -19,9 +19,31 @@ docker build -t dataflow/bison bison/
 
 # Fuzzing
 
+First, we need to pull out the files we need (don't fuzz inside the container).
+
 ```bash
 # Run the container
 docker run -ti --name bison-fuzz dataflow/bison
+
+# Setup experiment dir
+mkdir -p /path/to/experiment/dir/
+cd /path/to/experiment/dir
+git clone --depth=1 https://github.com/google/AFL
+make -C AFL -j && make -C AFL/llvm_mode -j
+git clone --depth=1 https://github.com/puppet-meteor/MOpt-AFL
+make -C MOpt-AFL/MOpt-AFL\ V1.0/ -j && make -C MOpt-AFL/MOpt-AFL\ V1.0/llvm_mode -j
+
+# Make target directory
+mkdir -p bison
+
+# Copy out files
+docker cp bison-fuzz:/root/bison-afl bison/
+docker cp bison-fuzz:/root/bison-datAFLow-access bison/
+docker cp bison-fuzz:/root/bison-datAFLow-access-idx bison/
+docker cp bison-fuzz:/root/seeds bison/
+
+# Run the experiment
+/path/to/this/dir/fuzz.sh bison
 ```
 
 Once everything is finished:
--- fuzzing-targets/fuzz.sh
+++ fuzzing-targets/fuzz.sh
@@ -1,67 +1,98 @@
 #!/bin/bash -ex
 
-. ${WORKDIR}/fuzz-config.sh
+if [ "$#" -ne 1 ]; then
+    echo "usage: $0 TARGET"
+    exit 1
+fi
+
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+TARGET=$1
+TIMEOUT=86400
+JOBS=30
+SEEDS="${TARGET}/seeds"
+
+export AFL_NO_UI=1
+. ${SCRIPT_PATH}/${TARGET}/fuzz-config.sh
 
 # AFL fuzz
 for I in $(seq 1 5); do
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/afl-$I.time   \
-        timeout --preserve-status 24h                               \
-        ${WORKDIR}/AFL/afl-fuzz -m none                             \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/afl-out-$I --         \
-            ${AFL_TARGET} ${TARGET_OPTS} > ${WORKDIR}/afl-$I.log 2>&1 &
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u    \
+    /usr/bin/time --verbose --output=${TARGET}/afl-$I.time              \
+    AFL/afl-fuzz -m none                                                \
+        -i ${SEEDS} -o ${TARGET}/afl-out-$I --                          \
+        ${TARGET}/${AFL_TARGET} ${TARGET_OPTS}                          \
+        > ${TARGET}/afl-$I.log 2>&1
     sleep 2
 done
 
 # MOpt fuzz
 for I in $(seq 1 5); do
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/mopt-$I.time  \
-        timeout --preserve-status 24h                               \
-        ${WORKDIR}/MOpt-AFL/MOpt-AFL\ V1.0/afl-fuzz -L 0 -m none    \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/mopt-out-$I           \
-            -- ${AFL_TARGET} ${TARGET_OPTS} > ${WORKDIR}/mopt-$I.log 2>&1 &
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u -q \
+    /usr/bin/time --verbose --output=${TARGET}/mopt-$I.time             \
+    "MOpt-AFL/MOpt-AFL V1.0/afl-fuzz" -L 0 -m none                      \
+        -i ${SEEDS} -o ${TARGET}/mopt-out-$I                            \
+        -- ${TARGET}/${AFL_TARGET} ${TARGET_OPTS}                       \
+        > ${TARGET}/mopt-$I.log 2>&1
     sleep 2
 done
 
+# Need to fix up the interpreter
+patchelf --set-interpreter                                  \
+    "${TARGET}/${DATAFLOW_ACCESS_LD}/ld-linux-x86-64.so.2"  \
+    ${TARGET}/${DATAFLOW_ACCESS_TARGET}
+
 # datAFLow access fuzz
 for I in $(seq 1 5); do
-    LD_LIBRARY_PATH=${DATAFLOW_ACCESS_LD}:${LD_LIBRARY_PATH}                    \
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/datAFLow-access-$I.time   \
-        timeout --preserve-status 24h                                           \
-        ${WORKDIR}/AFL/afl-fuzz -m none                                         \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/datAFLow-access-out-$I --         \
-            ${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS} > ${WORKDIR}/datAFLow-access-$I.log 2>&1 &
+    LD_LIBRARY_PATH=${TARGET}/${DATAFLOW_ACCESS_LD}:${LD_LIBRARY_PATH}  \
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u    \
+    /usr/bin/time --verbose --output=${TARGET}/datAFLow-access-$I.time  \
+    AFL/afl-fuzz -m none                                                \
+        -i ${SEEDS} -o ${TARGET}/datAFLow-access-out-$I --              \
+        ${TARGET}/${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS}              \
+        > ${TARGET}/datAFLow-access-$I.log 2>&1
     sleep 2
 done
 
 # datAFLow access MOpt fuzz
 for I in $(seq 1 5); do
-    LD_LIBRARY_PATH=${DATAFLOW_ACCESS_LD}:${LD_LIBRARY_PATH}                        \
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/datAFLow-access-mopt-$I.time  \
-        timeout --preserve-status 24h                                               \
-        ${WORKDIR}/MOpt-AFL/MOpt-AFL\ V1.0/afl-fuzz -L 0 -m none                    \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/datAFLow-access-mopt-out-$I --        \
-            ${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS} > ${WORKDIR}/datAFLow-access-mopt-$I.log 2>&1 &
+    LD_LIBRARY_PATH=${TARGET}/${DATAFLOW_ACCESS_LD}:${LD_LIBRARY_PATH}      \
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u -q     \
+    /usr/bin/time --verbose --output=${TARGET}/datAFLow-access-mopt-$I.time \
+    "MOpt-AFL/MOpt-AFL V1.0/afl-fuzz" -L 0 -m none                          \
+        -i ${SEEDS} -o ${TARGET}/datAFLow-access-mopt-out-$I --             \
+        ${TARGET}/${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS}                  \
+        > ${TARGET}/datAFLow-access-mopt-$I.log 2>&1
     sleep 2
 done
 
+# Need to fix up the interpreter
+patchelf --set-interpreter                                      \
+    "${TARGET}/${DATAFLOW_ACCESS_IDX_LD}/ld-linux-x86-64.so.2"  \
+    ${TARGET}/${DATAFLOW_ACCESS_IDX_TARGET}
+
 # datAFLow access + index fuzz
 for I in $(seq 1 5); do
-    LD_LIBRARY_PATH=${DATAFLOW_ACCESS_IDX_LD}:${LD_LIBRARY_PATH}                    \
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/datAFLow-access-idx-$I.time   \
-        timeout --preserve-status 24h                                               \
-        ${WORKDIR}/AFL/afl-fuzz -m none                                             \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/datAFLow-access-idx-out-$I --         \
-            ${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS} > ${WORKDIR}/datAFLow-access-idx-$I.log 2>&1 &
+    LD_LIBRARY_PATH=${TARGET}/${DATAFLOW_ACCESS_IDX_LD}:${LD_LIBRARY_PATH}  \
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u        \
+    timeout --preserve-status 24h                                           \
+    AFL/afl-fuzz -m none                                                    \
+        -i ${SEEDS} -o ${TARGET}/datAFLow-access-idx-out-$I --              \
+        ${TARGET}/${DATAFLOW_ACCESS_TARGET} ${TARGET_OPTS}                  \
+        > ${TARGET}/datAFLow-access-idx-$I.log 2>&1
     sleep 2
 done
 
 # datAFLow access + index MOpt fuzz
 for I in $(seq 1 5); do
-    LD_LIBRARY_PATH=${DATAFLOW_ACCESS_IDX_LD}:${LD_LIBRARY_PATH}                        \
-    nohup /usr/bin/time --verbose --output=${WORKDIR}/datAFLow-access-idx-mopt-$I.time  \
-        timeout --preserve-status 24h                                                   \
-        ${WORKDIR}/MOpt-AFL/MOpt-AFL\ V1.0/afl-fuzz -L 0 -m none                        \
-            -i ${WORKDIR}/seeds -o ${WORKDIR}/datAFLow-access-idx-mopt-out-$I --        \
-            ${DATAFLOW_ACCESS_IDX_TARGET} ${TARGET_OPTS} > ${WORKDIR}/datAFLow-access-idx-mopt-$I.log 2>&1 &
+    LD_LIBRARY_PATH=${TARGET}/${DATAFLOW_ACCESS_IDX_LD}:${LD_LIBRARY_PATH}      \
+    sem --timeout ${TIMEOUT} --jobs ${JOBS} --id "fuzz-${TARGET}" -u -q         \
+    /usr/bin/time --verbose --output=${TARGET}/datAFLow-access-idx-mopt-$I.time \
+    "MOpt-AFL/MOpt-AFL V1.0/afl-fuzz" -L 0 -m none                              \
+        -i ${SEEDS} -o ${TARGET}/datAFLow-access-idx-mopt-out-$I --             \
+        ${TARGET}/${DATAFLOW_ACCESS_IDX_TARGET} ${TARGET_OPTS}                  \
+        > ${TARGET}/datAFLow-access-idx-mopt-$I.log 2>&1
     sleep 2
 done
+
+# Wait for campaigns to finish
+sem --wait --id "fuzz-${TARGET}"
