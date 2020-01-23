a1bef46e62dffe4ede4f4047b078091abcf43935
Adrian Herrera <adrian.herrera02@gmail.com>
2020-01-24 10:10:53 +1100

    scripts: refactored analysis scripts to use Pandas

--- scripts/analysis/aggregate_plot_data.py
+++ scripts/analysis/aggregate_plot_data.py
@@ -2,6 +2,9 @@
 
 """
 Aggregate AFL plot data from repeated runs.
+
+This will normalize timestamps and combine all the plot data into a single CSV
+file.
 """
 
 from argparse import ArgumentParser
@@ -9,15 +12,13 @@ from collections import defaultdict
 import csv
 import os
 
+import pandas as pd
+
 
 PLOT_DATA_FIELDS = ('unix_time', 'cycles_done', 'cur_path', 'paths_total',
                     'pending_total', 'pending_favs', 'map_size',
                     'unique_crashes', 'unique_hangs', 'max_depth',
                     'execs_per_sec')
-OUT_PLOT_DATA_FIELDS = ('time',
-                        'cycles_done_min', 'map_size_min', 'unique_crashes_min',
-                        'cycles_done_avg', 'map_size_avg', 'unique_crashes_avg',
-                        'cycles_done_max', 'map_size_max', 'unique_crashes_max')
 
 
 def parse_args():
@@ -44,9 +45,8 @@ def main():
                             output_dir)
         plot_data_paths.add(plot_data)
 
-    num_campaigns = len(plot_data_paths)
     csv.register_dialect('afl_plot_data', delimiter=',', skipinitialspace=True)
-    results = defaultdict(lambda: defaultdict(dict))
+    plot_data = defaultdict(lambda: defaultdict(dict))
 
     for i, plot_data_path in enumerate(plot_data_paths):
         with open(plot_data_path, 'r') as plot_data_file:
@@ -64,34 +64,54 @@ def main():
 
                 time = unix_time - start_time
                 cycles_done = int(row['cycles_done'])
+                cur_path = int(row['cur_path'])
+                paths_total = int(row['paths_total'])
+                pending_total = int(row['pending_total'])
+                pending_favs = int(row['pending_favs'])
                 map_size = float(row['map_size'].split('%')[0])
                 unique_crashes = int(row['unique_crashes'])
+                unique_hangs = int(row['unique_hangs'])
+                max_depth = int(row['max_depth'])
+                execs_per_second = float(row['execs_per_sec'])
 
-                results[time]['cycles_done'][i] = cycles_done
-                results[time]['map_size'][i] = map_size
-                results[time]['unique_crashes'][i] = unique_crashes
+                plot_data[time]['cycles_done'][i] = cycles_done
+                plot_data[time]['cur_path'][i] = cur_path
+                plot_data[time]['paths_total'][i] = paths_total
+                plot_data[time]['pending_total'][i] = pending_total
+                plot_data[time]['pending_favs'][i] = pending_favs
+                plot_data[time]['map_size'][i] = map_size
+                plot_data[time]['unique_crashes'][i] = unique_crashes
+                plot_data[time]['unique_hangs'][i] = unique_hangs
+                plot_data[time]['max_depth'][i] = max_depth
+                plot_data[time]['execs_per_sec'][i] = execs_per_sec
 
+    cycles_done_key = lambda i: 'cycles_done_%d' % i
+    map_size_key = lambda i: 'map_size_%d' % i
+    unique_crashes_key = lambda i: 'unique_crashes_%d' % i
+
+    num_campaigns = len(plot_data_paths)
+    columns = ['time'] + \
+              [cycles_done_key(i) for i in range(0, num_campaigns)] + \
+              [map_size_key(i) for i in range(0, num_campaigns)] + \
+              [unique_crashes_key(i) for i in range(0, num_campaigns)]
+
+    aggregated_plot_data = defaultdict(list)
+
+    for time, data in sorted(plot_data.items()):
+        aggregated_plot_data['time'].append(time)
+
+        for i in range(0, num_campaigns):
+            cycles_done = data['cycles_done'].get(i)
+            map_size = data['map_size'].get(i)
+            unique_crashes = data['unique_crashes'].get(i)
+
+            aggregated_plot_data[cycles_done_key(i)].append(cycles_done)
+            aggregated_plot_data[map_size_key(i)].append(map_size)
+            aggregated_plot_data[unique_crashes_key(i)].append(unique_crashes)
+
+    df = pd.DataFrame(data=aggregated_plot_data).ffill()
     with open(args.output, 'w') as outf:
-        cycles_done = lambda i: 'cycles_done_%d' % i
-        map_size = lambda i: 'map_size_%d' % i
-        unique_crashes = lambda i: 'unique_crashes_%d' % i
-
-        fieldnames = ['time'] + \
-                     [cycles_done(i) for i in range(0, num_campaigns)] + \
-                     [map_size(i) for i in range(0, num_campaigns)] + \
-                     [unique_crashes(i) for i in range(0, num_campaigns)]
-
-        writer = csv.DictWriter(outf, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for time, data in sorted(results.items()):
-            d = dict(time=time)
-            for i in range(0, num_campaigns):
-                d[cycles_done(i)] = data['cycles_done'].get(i, '')
-                d[map_size(i)] = data['map_size'].get(i, '')
-                d[unique_crashes(i)] = data['unique_crashes'].get(i, '')
-
-            writer.writerow(d)
+        df.to_csv(outf, index=False)
 
 
 if __name__ == '__main__':
