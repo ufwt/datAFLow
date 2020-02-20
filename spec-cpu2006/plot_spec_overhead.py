44c814140616d379a693aeda76c5283ada791685
Adrian Herrera <adrian.herrera02@gmail.com>
2020-02-21 09:30:44 +1100

    spec-cpu2006: plot SPEC overhead

---
+++ spec-cpu2006/plot_spec_overhead.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
+
+rc('font', **{'family': 'serif', 'serif': ['Palatino']})
+rc('text', usetex=True)
+
+
+FUZZERS = {'afl': 'AFL',
+        'datAFLow-access': 'datAFLow (access)',
+        'datAFLow-access-idx': 'datAFLow (access + index)',
+        'angora-track': 'Angora',
+        }
+
+
+THIS_DIR = os.path.dirname(__file__)
+
+
+def main():
+    if len(sys.argv) < 2:
+        raise Exception('usage: %s /path/to/output/pdf' % sys.argv[0])
+
+    df = pd.read_csv(os.path.join(THIS_DIR, 'results', 'results.csv'))
+    fuzzer_cols = list(FUZZERS.keys())
+    df[fuzzer_cols] = df[fuzzer_cols].div(df.clang, axis=0)
+    df = df.drop('clang', axis=1)
+
+    num_targets, num_fuzzers = df.shape
+    index = np.arange(num_targets)
+    bar_width = 0.8 / num_fuzzers
+
+    # Plot
+
+    plt.style.use('ggplot')
+
+    for i, (fuzzer, label) in enumerate(FUZZERS.items()):
+        plt.bar(index + bar_width * i, df[fuzzer], width=bar_width,
+                label=label, log=True)
+
+    plt.xlabel('Benchmark')
+    plt.ylabel('Overhead (×)')
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=num_fuzzers)
+    plt.xticks(index + bar_width, df.target, rotation=90)
+
+    plt.tight_layout()
+    plt.savefig(sys.argv[1])
+
+
+if __name__ == '__main__':
+    main()
